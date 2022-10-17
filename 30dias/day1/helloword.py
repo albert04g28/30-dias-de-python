6537eb6f031f4f1ce45330815fbbba1e64e8fe37
Carlos <cbechur0606@m.educaand.es>
2022-10-17 12:06:58 +0200

    hello

---
+++ 30dias/day1/helloword.py
@@ -0,0 +1,30 @@
+
+
+
+#ejercicio 2 
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(4/3)
+print(3**4)
+
+#ejercicio 3
+
+print("Alberto")
+print("Aparicio")
+print("Spain")
+print("I am enjoying 30 days of python")
+
+#ejercicio 4
+
+print(type(10))          
+print(type(3.14))        
+print(type(4 - 4j))      
+print(type('Asabeneh',"python", "finland"))  
+print(type({'name':'Alberto'}))   
+print(type({'first name':'Aparicio'}))
+print(type({'Country':'Spain'}))
+
+
