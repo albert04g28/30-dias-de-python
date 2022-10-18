977781f1cf86a7fbffb90df509b035027bebe64c
Carlos <cbechur0606@m.educaand.es>
2022-10-18 13:41:05 +0200

    hola

---
+++ 30dias/day2/variables.py
@@ -0,0 +1,20 @@
+#level 1
+#Day 2: 30 Days of python programming
+
+first_name="Alberto"
+last_name="Apario"
+full_name="Alberto_Aparicio"
+country="España"
+city="Jerez"
+age="17"
+year="2022"
+is_married="no"
+is_true="si"
+is_light_on="si"
+letras="a","b"
+
+#level 2
+
+
+
+    
