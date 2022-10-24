49688fd9db6161fc45e7f60d6201c78bfb0d1eff
Carlos <cbechur0606@m.educaand.es>
2022-10-24 12:40:00 +0200

    hola

--- 30dias/day2/variables.py
+++ 30dias/day2/variables.py
@@ -1,13 +1,13 @@
 #level 1
 #Day 2: 30 Days of python programming
-
+#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
 first_name="Alberto"
 last_name="Apario"
 full_name="Alberto_Aparicio"
 country="España"
 city="Jerez"
-age="17"
-year="2022"
+age=17
+year=2022
 is_married="no"
 is_true="si"
 is_light_on="si"
@@ -15,6 +15,33 @@ letras="a","b"
 
 #level 2
 
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(letras))
 
 
-    
+
+
+print(len(first_name))
+print(len(last_name))
+
+
+
+num_one=5 
+num_two=4
+num_total=num_one+num_two
+print(num_total)
+num_diff=num_one-num_two
+print(num_diff)
+num_product=num_one * num_two
+print(num_product)
+num_divide=num_one/num_two
+print(num_divide)
